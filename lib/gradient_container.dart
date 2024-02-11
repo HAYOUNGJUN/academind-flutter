c2e39eb8057b422ecea6a59f1dda2f71aa79f4a0
Youngjun Ha <dudwnsk011@gmail.com>
2024-02-11 20:03:13 +0900

    flutter basics

---
+++ lib/gradient_container.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:bascis/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+
+  // final List<Color> colors;
+  // ㄴ--> {super.key, required this.colors}
+  // @override
+  // Widget build(context) {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //       gradient: LinearGradient(
+  //         colors: colors,
+  //         begin: startAlignment,
+  //         end: endAlignment,
+  //       ),
+  //     ),
+  //     child: const Center(
+  //       child: StyledText('Hello World!!!'),
+  //     ),
+  //   );
+  // }
+}

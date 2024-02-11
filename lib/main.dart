76d4ad5fa113e67a4b649a02b0ea90c9c7af03e5
Youngjun Ha <dudwnsk011@gmail.com>
2024-02-11 21:01:33 +0900

    Scratching the first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.indigo],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(color: Colors.white, fontSize: 28),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Start Quiz',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }

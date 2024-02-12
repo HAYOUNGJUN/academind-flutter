3bf58c0f881906b1d2435ced727836d95bd28146
Youngjun Ha <dudwnsk011@gmail.com>
2024-02-12 10:09:03 +0900

    Starting setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(MaterialApp(home: ,),);
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
-
